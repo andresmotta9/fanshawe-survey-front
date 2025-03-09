6362ada2e2f1fde46219778c2a2f4f00b01a0567
Bankole Theophilus <71837809+phynnoex@users.noreply.github.com>
2025-03-08 20:05:18 -0500

    Add HeroSection component and update styles for buttons and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import useFetch from './hooks/useFetch';
 import HeaderSection from './features/headerSection';
+import HeroSection from './features/heroSection';
 
 function App() {
   const { data: users, loading, error } = useFetch('USERS');
   return (
     <div>
       <HeaderSection />
+      <HeroSection />
     </div>
       
   );
