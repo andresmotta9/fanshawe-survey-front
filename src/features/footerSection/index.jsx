364e0482ff16133e3902fa5dda8efc11a02966dd
Jarvis <meetvyas1110@gmail.com>
2025-03-11 21:54:32 -0400

    Make footer logo clickable to reload page and update contact email address

--- src/features/footerSection/index.jsx
+++ src/features/footerSection/index.jsx
@@ -7,7 +7,9 @@ const Footer = () => {
       <div className="footer-container">
         {/* Logo & Socials */}
         <div className="footer-logo">
-          <span className="logo-text">FanSurvey.</span>
+ <span className="logo-text" onClick={() => window.location.reload()} style={{ cursor: 'pointer' }}>
+                    FanSurvey
+                </span>
           <div className="social-icons">
             <a href="#" className="circle"></a>
             <a href="#" className="circle"></a>
@@ -18,7 +20,7 @@ const Footer = () => {
         {/* Contact Section */}
         <div className="footer-contact">
           <h4>CONTACT US</h4>
-          <p><a href="mailto:Bankoletheophilus21@gmail.com">Bankoletheophilus21@gmail.com</a></p>
+          <p><a href="mailto:fansurveys25@gmail.com">fansurvey25@gmail.com</a></p>
           <p>Providing IT program recommendations based on your skills & interests.</p>
           <p><a href="tel:+12268833114">+1 (226)-883-3114</a></p>
         </div>
@@ -28,7 +30,7 @@ const Footer = () => {
           <h4>SUBSCRIBE</h4>
           <p>Enter your email to get updates on Fanshawe IT courses and recommendations</p>
           <form>
-            <input type="email" placeholder="example@gmail.com" />
+            <input  type="email" placeholder="example@gmail.com" />
             <button type="submit">Subscribe</button>
           </form>
         </div>
