2add232ccccb992f478a1524d489267e91b0d6a3
Jarvis <meetvyas1110@gmail.com>
2025-03-11 22:21:03 -0400

    Add FooterSection component and update HeroSection text formatting

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import useFetch from './hooks/useFetch';
 import HeaderSection from './features/headerSection';
 import HeroSection from './features/heroSection';
 import CoursesSection from './features/coursesSection';
-import BannerSection from './features/bannerSection';
+import FooterSection from './features/footerSection';
 
 function App() {
   const { data: users, loading, error } = useFetch('USERS');
@@ -12,6 +12,7 @@ function App() {
       <HeaderSection />
       <HeroSection />
       <CoursesSection />
+      <FooterSection />
     </div>
       
   );
