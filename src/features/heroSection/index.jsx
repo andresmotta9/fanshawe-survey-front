184d596620145ee6c01949a07f8de85ca4c9a1a2
Jarvis <meetvyas1110@gmail.com>
2025-03-13 23:37:33 -0400

    Capitalize button names in HeroSection for consistency

--- src/features/heroSection/index.jsx
+++ src/features/heroSection/index.jsx
@@ -14,8 +14,8 @@ export default function HeroSection(props) {
                 Discover the best IT diploma program at Fanshawe College based on your skills,provisions, interests, and career goals. Take our quick Quiz and get personalized recommendations!
             </div>
             <div className="buttonContainer">
-                <SecondaryButton name="view Courses"/>
-                <PrimaryQuizButton name="start Quiz"/>   
+                <SecondaryButton name="View Courses"/>
+                <PrimaryQuizButton name="Start Quiz"/>   
             </div>
             <div className="Testimonials">
                 <div className="TestimonialAvatarContainer">
