4b9bb45a0697885d1309dfe906dbefbc89d1a1e7
Bankole Theophilus <71837809+phynnoex@users.noreply.github.com>
2025-03-15 01:19:36 -0400

    Enhance BannerSection with responsive design adjustments and improved layout for tablet and desktop views

--- src/features/bannerSection/index.jsx
+++ src/features/bannerSection/index.jsx
@@ -2,8 +2,11 @@ import "./styles.css";
 import React from 'react';
 import { motion } from "framer-motion";
 import PrimaryQuizButton from "../../components/primaryQuizButton";
+import { useMediaQuery } from "react-responsive";
 
 export default function BannerSection() {
+    // const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+    const isTablet = useMediaQuery({ query: '(min-width: 1024px)' });
     return (
         <div className="bannerSection">
             <div className="bannerSectionImageContainer">
@@ -20,9 +23,9 @@ export default function BannerSection() {
                 <div className="spiralBind__spiral"></div>  
             </div>
             <motion.div className="bannerSection__content"
-                initial={{ opacity: 0, rotateX: 90 }}
+                initial={{ opacity: 0, rotateX: isTablet ? 0 : 90, rotateY: isTablet ? -90 : 0 }}
                 // animate={{ opacity: 1, rotateX: 0 }}
-                whileInView={{ opacity: 1, rotateX: 0 }}
+                whileInView={{ opacity: 1, rotateX: isTablet ? 0 : 0, rotateY: isTablet ? 0 : 0 }}
                 transition={{ duration: 1 }}
             >
                 <div className="holesContainer top">
