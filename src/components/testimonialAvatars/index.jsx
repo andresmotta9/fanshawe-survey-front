35e4016e58200b018f7d05908fc5d736085ee07b
andresmotta9 <46831406+andresmotta9@users.noreply.github.com>
2025-03-17 16:15:54 -0400

    Revert "Feature/courses api integration"

--- src/components/testimonialAvatars/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./styles.css"
-
-export default function TestimonialAvatar(props) {
-    return (
-        <div className="testimonialAvatar">
-            {/* <img src={props.avatar} alt="Avatar" width={100} height={100} /> */}
-        </div>
-    )
-}
--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,21 +1,11 @@
 const API_BASE_URL = 'http://localhost:3000/api';
 
 const API_ENDPOINTS = {
-  USERS: `${API_BASE_URL}/users`,
+  USERS: `${API_BASE_URL}/users`, // Fetch all users (admins in this case)
   LOGIN: `${API_BASE_URL}/auth/login`,
   DIPLOMAS: `${API_BASE_URL}/diplomas`,
   QUESTIONS: `${API_BASE_URL}/questions`,
   ANSWERS: `${API_BASE_URL}/answers`,
-  COURSES: `${API_BASE_URL}/courses`,
-};
-
-export const API_KEYS = {
-  USERS: 'USERS',
-  LOGIN: 'LOGIN',
-  DIPLOMAS: 'DIPLOMAS',
-  QUESTIONS: 'QUESTIONS',
-  ANSWERS: 'ANSWERS',
-  COURSES: 'COURSES',
 };
 
 export default API_ENDPOINTS;
