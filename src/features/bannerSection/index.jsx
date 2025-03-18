3b602d7f6ef11796114877c9b3f14a0a95195df6
Bankole Theophilus <71837809+phynnoex@users.noreply.github.com>
2025-03-18 15:18:29 -0400

    Update viewport settings in BannerSection for improved visibility threshold

--- src/features/bannerSection/index.jsx
+++ src/features/bannerSection/index.jsx
@@ -23,7 +23,7 @@ export default function BannerSection() {
                 <div className="spiralBind__spiral"></div>  
             </div>
             <motion.div className="bannerSection__content"
-                viewport={{ once: true }}
+                viewport={{ once: true, amount: 0.2 }}
                 initial={{ opacity: 0, rotateX: isTablet ? 0 : 90, rotateY: isTablet ? -90 : 0 }}
                 // animate={{ opacity: 1, rotateX: 0 }}
                 whileInView={{ opacity: 1, rotateX: isTablet ? 0 : 0, rotateY: isTablet ? 0 : 0 }}
