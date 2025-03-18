8176f3a3902efb8d635aadf9f1d01c0d5a87daae
Bankole Theophilus <71837809+phynnoex@users.noreply.github.com>
2025-03-18 16:24:51 -0400

    instructions page

---
+++ src/InstructionsPage/index.js
@@ -0,0 +1,7 @@
+export default function InstructionPage(props) {
+    return (
+        <div className="instructionPage">
+            
+        </div>
+    )
+}
--- src/components/HeroPageVisulas/index.jsx
+++ src/components/HeroPageVisulas/index.jsx
@@ -3,22 +3,47 @@ import arcs from ".//imgVisuals/arcs.png";
 import bright from ".//imgVisuals/bright.png";
 import security from ".//imgVisuals/security.png";
 import settings from ".//imgVisuals/settings.png";
+import {motion} from "framer-motion";
 
 export default function HeroPageVisuals() {
   return (
     <div className="heroVisuals">
-      <div className="firstVisual">
+      <motion.div className="firstVisual"
+        initial={{  y: -10, rotateY: 35 }}
+        animate={{   y: 0, rotateY: 0 }}
+        //loop
+        transition={{ duration: 1, repeat: Infinity, repeatType: "reverse", delay: 1, ease: "easeInOut" }}
+        
+      >
         <img src={bright} alt="" />
-      </div>
-      <div className="SecondVisual">
+      </motion.div>
+      <motion.div className="SecondVisual"
+        initial={{  y: -10, rotateY: -25, rotateX: -15 }}
+        animate={{   y: 0, rotateY: 0, rotateX: 0 }}
+        
+        //loop
+        transition={{ duration: 1, repeat: Infinity, repeatType: "reverse", ease: "easeInOut" }}
+      >
         <img src={security} alt="" />
-      </div>
-      <div className="ThirdVisual">
+      </motion.div>
+      <motion.div className="ThirdVisual"
+        initial={{  y: -10, rotateY: -25, rotateX: 15 }}
+        animate={{   y: 0, rotateY: 0, rotateX: 0 }}
+        
+        //loop
+        transition={{ duration: 1, repeat: Infinity, repeatType: "reverse", ease: "easeInOut" }}
+      >
         <img src={settings} alt="" />
-      </div>
-      <div className="FourthVisual">
+      </motion.div>
+      <motion.div className="FourthVisual"
+      
+      initial={{  y: -10, rotateY: -25, rotateX: 15 }}
+      animate={{   y: 0, rotateY: 0, rotateX: 0 }}
+      
+      //loop
+      transition={{ duration: 1, repeat: Infinity, repeatType: "reverse", ease: "easeInOut" }}>
         <img src={arcs} alt="" />
-      </div>
+      </motion.div>
     </div>
   );
 }
